e47b22ec8d7819b1700d99e4b3cdcd8ca7473ef9
hsannav <121253957+hsannav@users.noreply.github.com>
2025-11-17 23:07:37 +0100

    Centroides de zonas

---
+++ datalake/bronze/bronze_zona_distritos_centroides.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS bronze_zona_distritos_centroides (
+    distrito_id INTEGER,
+    longitude FLOAT,
+    latitude FLOAT
+)
---
+++ datalake/bronze/bronze_zona_gaus_centroides.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS bronze_zona_gaus_centroides (
+    gaus_id INTEGER,
+    longitude FLOAT,
+    latitude FLOAT
+)
